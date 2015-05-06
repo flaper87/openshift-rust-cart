e20a9d0106d5ca660dce52fc31680b18550201d3
Flavio Percoco <flaper87@gmail.com>
2015-05-07 00:44:30 +0200

    Rumble 20

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use] extern crate nickel;
+
+use std::env;
+use nickel::Nickel;
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+
+    let addr = format!("{}:{}", env::var("HOST").unwrap(), env::var("PORT").unwrap());
+    server.listen(&*addr);
 }
